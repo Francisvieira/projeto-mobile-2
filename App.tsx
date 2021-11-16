9e0612d2a77db35a8362c9aa778f3e052f8c9d03
francisco <fvieira340@gmail.com>
2021-11-15 22:19:24 -0300

    formatar

--- App.tsx
+++ App.tsx
@@ -16,24 +16,25 @@ export default function App() {
 
       {/*====================inicio da imagem *====================*/}
 
-      <View style={estilo.img}>
-        <Image source={{ uri: "https://wakke.co/wp-content/uploads/2018/08/escolaweb-capas-artigos-qual-a-real-importancia-do-marketing-educacional-para-minha-ie-1.jpg" }} style={estilo.imgedu} />
-      </View>
+     
+
 
 
 
       {/*=========================rolagem ====================*/}
       <ScrollView horizontal={false}>
-
+      <View style={estilo.img}>
+        <Image source={{ uri: "https://wakke.co/wp-content/uploads/2018/08/escolaweb-capas-artigos-qual-a-real-importancia-do-marketing-educacional-para-minha-ie-1.jpg" }} style={estilo.imgedu} />
+      </View>
 
         <View style={estilo.prof}>
           <Image source={{ uri: "https://st2.depositphotos.com/1017228/11660/i/600/depositphotos_116601598-stock-photo-full-length-portrait-of-man.jpg" }} style={estilo.foto} />
           <View>
-            <Text>fulano de tal</Text>
-            <Text>matematica</Text>
+            <Text style={estilo.texto1}>Rodrigo</Text>
+            <Text style={estilo.texto2}>Matematica</Text>
           </View>
           <View>
-            <Text> 50R$ por h</Text>
+            <Text style={estilo.texto3}> 50R$ por h</Text>
           </View>
         </View>
 
@@ -41,33 +42,33 @@ export default function App() {
         <View style={estilo.prof}>
           <Image source={{ uri: "https://cdn.w600.comps.canstockphoto.com.br/-pessoas-isolado-americano-pretas-banco-de-imagem_csp40184869.jpg" }} style={estilo.foto} />
           <View>
-            <Text>fulano de tal</Text>
-            <Text>matematica</Text>
+            <Text style={estilo.texto4}>Henrique</Text>
+            <Text style={estilo.texto5}>Geografia</Text>
           </View>
-          <View>
-            <Text> 50R$ por h</Text>
+          <View >
+          <Text style={estilo.texto6}> 50R$ por h</Text>
           </View>
         </View>
 
         <View style={estilo.prof}>
           <Image source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrW3dfTSSKzLIVcByUADw6A7XHz6XMXayW6A&usqp=CAU" }} style={estilo.foto} />
           <View>
-            <Text>fulano de tal</Text>
-            <Text>matematica</Text>
+            <Text style={estilo.texto7}>Fernanda</Text>
+            <Text style={estilo.texto8}>Matematica</Text>
           </View>
           <View>
-            <Text> 50R$ por h</Text>
+            <Text style={estilo.texto9}> 50R$ por h</Text>
           </View>
         </View>
 
         <View style={estilo.prof}>
           <Image source={{ uri: "https://static.vecteezy.com/ti/fotos-gratis/p1/2010315-homem-bonito-asiatico-com-bigode-sorrindo-sobre-fundo-branco-gr%C3%A1tis-foto.jpg" }} style={estilo.foto} />
           <View>
-            <Text>fulano de tal</Text>
-            <Text>matematica</Text>
+            <Text style={estilo.texto10}>Roger</Text>
+            <Text style={estilo.texto11}>Ingles</Text>
           </View>
           <View>
-            <Text> 50R$ por h</Text>
+            <Text style={estilo.texto12}> 30R$ por h</Text>
           </View>
         </View>
 
@@ -75,11 +76,11 @@ export default function App() {
         <View style={estilo.prof}>
           <Image source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCuljGRMkNhnzsLplTALkcNP3h_zIyRQkYOA&usqp=CAU" }} style={estilo.foto} />
           <View>
-            <Text >fulano de tal</Text>
-            <Text>matematica</Text>
+            <Text style={estilo.texto1} >Marcia</Text>
+            <Text style={estilo.texto2}>matematica</Text>
           </View>
           <View>
-            <Text> 50R$ por h</Text>
+            <Text style={estilo.texto3}> 50R$ por h</Text>
           </View>
         </View>
 
@@ -87,9 +88,26 @@ export default function App() {
 
 
       </ScrollView>
-     {/* ================================================fim da rolagem *=================================*/}
+      {/* ================================================fim da rolagem *=================================*/}
 
 
+
+      {/*===================================roda pé===========================*/}
+      <View style={estilo.footer}>
+        
+        <Image source={{uri:"https://www.iconpacks.net/icons/2/free-home-icon-2502-thumb.png"}} style={estilo.fotofooter0}/>
+        
+
+        <Image source={{uri:"https://www.iconpacks.net/icons/1/free-compass-icon-881-thumb.png"}} style={estilo.fotofooter1}/>
+        
+        <Image source={{uri:"https://cdn-icons-png.flaticon.com/512/1946/1946429.png"}} style={estilo.fotofooter2}/>
+        
+        <Image source={{uri:"https://www.iconpacks.net/icons/2/free-settings-icon-3110-thumb.png"}} style={estilo.fotofooter3}/>
+      
+      </View> 
+
+
+      
 
 
 
@@ -102,6 +120,8 @@ export default function App() {
 
 
     </View>
+
+
 
   );
 
--- css/Styles.tsx
+++ css/Styles.tsx
@@ -7,29 +7,29 @@ export const estilo = StyleSheet.create({
 
     },
     box: {
-        flexDirection:"row",
-       
-        alignItems:"center",
-        alignContent:"center",
-        borderColor:'black',
-        borderWidth:1
-        
+        flexDirection: "row",
+
+        alignItems: "center",
+        alignContent: "center",
+        borderColor: 'black',
+        borderWidth: 1
+
     },
 
     icone: {},
 
     imgicone: {
-        height:50,
-        width:50,
-        position:"absolute",
-        right:5,
-        top:50
+        height: 50,
+        width: 50,
+        position: "absolute",
+        right: 5,
+        top: 50
     },
-    pesquisar:{
-        padding:10,
-        backgroundColor:'lightgrey',
-        paddingTop:20,
-        paddingBottom:20,
+    pesquisar: {
+        padding: 10,
+        backgroundColor: 'lightgrey',
+        paddingTop: 20,
+        paddingBottom: 20,
         width: '80%',
         marginLeft: 10,
         marginRight: 30,
@@ -39,28 +39,167 @@ export const estilo = StyleSheet.create({
 
 
     //imagem abaixo//
-    img:{
-        paddingTop:10,
-        paddingLeft:10
+    img: {
+        paddingTop: 10,
+        paddingLeft: 10
     },
-    imgedu:{
-        
-        height:200,
-        width:350
+    imgedu: {
+
+        height: 200,
+        width: 350
 
     },
     // barra de rolgem //
-  
-    foto:{
-        height:50,
-        width:50
+
+    foto: {
+        height: 50,
+        width: 50,
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: 'lightgrey'
     },
-    prof:{
-        
+    prof: {
+        paddingTop: 30,
+        flexDirection: "row",
+        borderWidth: 1,
+        borderColor: 'lightgrey',
+        borderRadius: 10
+
+
+    },
+    texto1: {
+        fontSize:30,
+        alignItems: "center",
+        alignContent: "center",
+        flexDirection: "row",
+        margin: 10,
+        left: 10,
+        bottom: 10
+    },
+    texto2: {
+        alignItems: "center",
+        alignContent: "center",
+        left: 10,
+        bottom: 10
+    },
+    texto3: {
+        alignItems: "center",
+        alignContent: "center",
+        left: 120,
+        position:"absolute"
+    },
+    texto4: {
+        fontSize:25,
+        alignItems: "center",
+        alignContent: "center",
+        flexDirection: "row",
+        margin: 10,
+        left: 20,
+        bottom: 10
+
+    }, texto5: {
+        alignItems: "center",
+        alignContent: "center",
+        left: 20,
+        bottom: 10
+
+    }, texto6: {
+        alignItems: "center",
+        alignContent: "center",
+        left: 120,
+        position:"absolute"
+
+    }, 
+    texto7: { 
+    fontSize:25,
+    alignItems: "center",
+    alignContent: "center",
+    flexDirection: "row",
+    margin: 10,
+    left: 20,
+    bottom: 10
+
+    }, 
+    texto8: { alignItems: "center",
+    alignContent: "center",
+    left: 10,
+    bottom: 10
+
+    }, 
+    texto9: {alignItems: "center",
+    alignContent: "center",
+    left: 120,
+    position:"absolute"
+
+    }, 
+    texto10: {
+    fontSize:25,
+    alignItems: "center",
+    alignContent: "center",
+    flexDirection: "row",
+    margin: 10,
+    left: 20,
+    bottom: 10
+
+    }, 
+    texto11: { alignItems: "center",
+    alignContent: "center",
+    left: 20,
+    bottom: 10
+
+    },
+     texto12: { alignItems: "center",
+     alignContent: "center",
+     left: 150
+
+    },
+
+
+    footer: {
+        width: '100%',
+        height: 80,
+        backgroundColor: 'white',
+        borderRadius: 1,
+        borderColor: 'black',
+        borderWidth: 1,
+        flexDirection: "row",
+        paddingLeft: 20
+    },
+    fotofooter0: {
+        height: 40,
+        width: 40,
+        margin: 20,
+        right: 20,
+        bottom: 10
+
+    },
+    fotofooter1: {
+        height: 40,
+        width: 40,
+        margin: 20,
+        left: 0,
+        bottom: 10
+    },
+    fotofooter2: {
+        height: 40,
+        width: 40,
+        margin: 20,
+        bottom: 10,
+        left: 20
+
+    },
+    fotofooter3: {
+        height: 40,
+        width: 40,
+        margin: 20,
+        bottom: 10,
+        left: 25
     }
 
 
-    
 
-    
+
+
+
+
 })
